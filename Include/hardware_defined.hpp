474b9d457e4fe324d9d52006335e67d2c3d3d9af
Favrou jérôme <38081092+jeromefavrou@users.noreply.github.com>
2019-07-12 17:09:12 +0200

    Add files via upload

--- Include/hardware_defined.hpp
+++ Include/hardware_defined.hpp
@@ -27,7 +27,7 @@ class Rasp_version : public Nocopyable
 
 typedef volatile uint32_t * Hardware_ptr;
 
-typedef enum class HARDWARE_VERSION  : int const {BCM2835=0,BCM2836,BCM2837,BCM2837B0,} Soc_hardware_version;
+typedef enum class HARDWARE_VERSION  : int const {BCM2835=0,BCM2836,BCM2837,BCM2837B0,BCM2711} Soc_hardware_version;
 
 typedef enum class PO_GP : int const {GPFSEL=0,GPSET,GPCLR,GPLEV,GPEDS,GPREN,GPFEN,GPHEN,GPLEN,GPAREN,GPAFEN,GPPUD,GPPUDCLK,} SoC_gp_physical_offset;
 
@@ -37,6 +37,6 @@ typedef enum class FSEL : std::uint8_t const {INPUT = 0,OUTPUT ,ALT0, ALT1 ,ALT2
 
 typedef enum class LEV : std::uint8_t const {LOW=0x00,HIGH=0x01,}Level;
 
-typedef enum class BAUD : unsigned long const{BR_9600=9600,} Baud_Rate;
+typedef enum class BAUD : unsigned long const{BR_9600=9600,BR_19200=19200,BR_57600=57600,BR_115200=115200,} Baud_Rate;
 
 #endif // HARDWARE_DEFINED_HPP_INCLUDED
--- Include/uart.hpp
+++ Include/uart.hpp
@@ -13,18 +13,22 @@
 #include <sys/select.h>
 #include <sys/time.h>
 #include <sys/ioctl.h>
-#include <asm/termbits.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <chrono>
 #include <thread>
+#include <termios.h>
+#include <unistd.h>
 
-extern "C" int tcdrain (int __fd);
 
 class Serial : public Nocopyable
 {
 public:
+
+    typedef std::vector<char> Data;
+
+
 
     class Serial_Error :public Error
     {
@@ -33,156 +37,30 @@ public:
         virtual ~Serial_Error(){}
     };
 
-    typedef std::vector<char> Data;
+    Serial(std::string const & , Baud_Rate const & );
 
-    Serial(std::string  const & pth_driver, Baud_Rate const & br):m_driver_path(pth_driver),m_baud_rate(br),m_init(false),m_fd(-1)
-    {
-    }
-    ~Serial(void)
-    {
-        this->end();
-    }
+    void flush (void);
 
-    void init(void)
-    {
-        struct termios2 options;
+    void write(std::string const & );
 
-        if (this->m_fd >= 0)
-            this->end();
+    std::string read(void);
 
-        if(!this->check_file(this->m_driver_path))
-            throw Serial_Error(0,"driver path unfound",Error::level::FATAL_ERROR);
+    char read_c(void);
 
-        if(system(nullptr));
-        else
-            throw  Serial_Error(1,"system() not ready",Error::level::FATAL_ERROR);
+    int availble(void);
 
-        this->m_fd = open(this->m_driver_path.c_str(), O_RDWR|O_NOCTTY);
-
-        if (this->m_fd < 0)
-            return ;
-
-
-        fcntl(this->m_fd, F_SETFL, 0);
-
-        ioctl(this->m_fd, TCGETS2, &options);
-        ioctl(this->m_fd, TCGETS2, &this->m_savedOptions);
-        options.c_cflag &= ~CBAUD;
-        options.c_cflag |= BOTHER;
-        options.c_ispeed = static_cast<unsigned long>(this->m_baud_rate);
-        options.c_ospeed = static_cast<unsigned long>(this->m_baud_rate);
-        this->m_savedOptions.c_cflag |= BOTHER;
-        this->m_savedOptions.c_ispeed = static_cast<unsigned long>(this->m_baud_rate);
-        this->m_savedOptions.c_ospeed = static_cast<unsigned long>(this->m_baud_rate);
-        options.c_cflag |= (CLOCAL | CREAD);
-        options.c_cflag &= ~CRTSCTS;
-        options.c_cflag &= ~HUPCL;
-        this->m_savedOptions.c_cflag &= ~HUPCL;
-
-        options.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP
-                    | INLCR | IGNCR | ICRNL | IXON);
-        options.c_oflag &= ~OPOST;
-        options.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
-        options.c_cflag &= ~(CSIZE | PARENB);
-        options.c_cflag |= CS8;
-
-        if (strcmp(this->m_driver_path.c_str(), "/dev/tty") == 0)
-            options.c_lflag |= ISIG;
-
-        ioctl(this->m_fd, TCSETS2, &options);
-
-        this->m_init=true;
-    }
-
-    char read(void)
-    {
-        if (!this->available())
-            return char(0x00);
-
-        char *c;
-
-        if (::read(this->m_fd, c, 1) <= 0)
-            return char(0x00);
-
-        return *c;
-    }
-
-    std::string read_str(char ending,double ms_delay)
-    {
-        std::string res{""};
-
-        auto start = std::chrono::system_clock::now();
-
-        while(std::chrono::duration<double,std::milli>(std::chrono::system_clock::now()-start).count()<=ms_delay)
-        {
-            char tps=this->read();
-
-            if(tps==ending)
-                break;
-            else
-                res+=tps;
-        }
-
-        return res;
-    }
-
-    void write(Data const & msg)
-    {
-        if (this->m_fd < 0 || !this->m_init)
-            return;
-
-        ::write(this->m_fd, msg.data() , msg.size());
-    }
-    void write_str(std::string const & msg)
-    {
-        this->write(Data(msg.begin(),msg.end()));
-    }
-
-    inline int getFD() { return this->m_fd; }
+    ~Serial();
 
 private:
 
-    bool check_file(std::string const & str)
-    {
-        std::fstream If(str,std::ios::in|std::ios::out);
+    speed_t cast_baud_rate(Baud_Rate const & __speed);
 
-        return (If && !If.bad() && !If.fail())?true:false;
-    }
+    void throwing(void);
 
-    void end()
-    {
-        if (this->m_fd > -1)
-        {
-            tcdrain(this->m_fd);
-            ioctl(this->m_fd, TCSETS2, &this->m_savedOptions);
-            ::close(this->m_fd);
-        }
-        this->m_fd = -1;
-    }
+    bool m_file_desciptor_check,m_baud_rate_check;
 
-    bool available(void)
-    {
-        if (this->m_fd < 0 || !m_init)
-            return false;
+    int m_file_desciptor,m_stat;
 
-        fd_set rfds;
-        struct timeval tv;
+    speed_t m_speed;
 
-        FD_ZERO(&rfds);
-        FD_SET(this->m_fd, &rfds);
-        tv.tv_sec = 0;
-        tv.tv_usec = 1;
-        int retval = select(this->m_fd+1, &rfds, NULL, NULL, &tv);
-        if (retval)
-            return true;
-
-        return false;
-    }
-
-    bool m_init;
-    std::string m_driver_path;
-    Baud_Rate m_baud_rate;
-
-    int m_fd;
-    struct termios2 m_savedOptions;
 };
