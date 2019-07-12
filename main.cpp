35a29e04d0d1b36c9513a51070c773591750a389
Favrou jérôme <38081092+jeromefavrou@users.noreply.github.com>
2019-07-12 18:08:57 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,10 +11,6 @@ int main()
 
     try
     {
-        std::cout << "opening serial on 9600 br"<< std::endl;
-
-        serial0.init();
-
         std::cout << "opening physicals address: 0x"<< std::hex << gpio_mem->get_peripheral_addr()+gpio_mem->get_pa(PA::GPIO)<< " to 0x" <<gpio_mem->get_peripheral_addr()+gpio_mem->get_pa(PA::GPIO) + gpio_mem->get_block_size() <<std::endl<<std::endl;
 
         gpio_mem->init(PA::GPIO);
@@ -27,18 +23,41 @@ int main()
             return -1;
     }
 
-   GPIO<HARDWARE_VERSION::BCM2837B0,Rasp_version::PI_3BP::GPIO_4> gp_test{gpio_mem};
+    GPIO<HARDWARE_VERSION::BCM2837B0,Rasp_version::PI_3BP::GPIO_4> gp_test{gpio_mem};
 
     gp_test.direction(FSEL::OUTPUT);
 
-    for(auto i=0;i<10;i++)
+    for(auto i=0;i<100;i++)
     {
         gp_test.set(i%2?LEV::HIGH:LEV::LOW);
+
+        std::this_thread::sleep_for(std::chrono::duration<int,std::milli>(100));
     }
 
-
-
     gp_test.direction(FSEL::MASK);
+
+    try
+    {
+       serial0.write("test_en");
+
+       std::this_thread::sleep_for(std::chrono::duration<int,std::milli>(50));
+
+       int len=serial0.availble();
+
+       for(;len>0;len--)
+       {
+           std::cout << serial0.read_c();
+       }
+
+       std::cout << std::endl;
+    }
+    catch(Error & e)
+    {
+        std::cout << e.what() << std::endl;
+
+        if(e.get_level()!=Error::level::WARNING)
+            return -1;
+    }
 
     return 0;
 }
